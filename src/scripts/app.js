cfd36c4e50c826b220e87ccaeaaa3f491e7e3ff1
Pitonsio <ivaniglean@gmail.com>
2026-09-01 23:19:26 +0200

    Estaciones llenas, vacias y buscador de estaciona añadidos
    Se han añadido nuevas funcionalidades a la web

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -10,6 +10,8 @@ document.getElementById('huecos_totales').textContent = 'Huecos libres: ' + data
 document.getElementById('capacidad_total').textContent = 'Capacidad total: ' + dataSummary.totalCapacity;
 
 
+
+
 // Definimos el ranking de las estaciones con más bicis disponibles
 const responseRanking = await fetch('http://localhost:8080/station/ranking');
 const dataRanking = await responseRanking.json();
@@ -24,3 +26,77 @@ dataRanking.forEach(estacion => {
     i++;
 
 });
+
+
+
+// Definimos las estaciones que están llenas
+const responseFullStations = await fetch('http://localhost:8080/station/full');
+const dataFullStations = await responseFullStations.json();
+
+dataFullStations.forEach(estacion => {
+
+    const elemento = document.createElement('div'); // creamos el contenedor de cada una de las estaciones
+
+    elemento.textContent = estacion.name + ': ' + estacion.availableBikes + ' bicis disponibles';
+    document.getElementById('full-stations').appendChild(elemento);
+    i++;
+
+});
+
+
+
+// Definimos las estaciones que están vacías
+const responseEmptyStations = await fetch('http://localhost:8080/station/empty');
+const dataEmptyStations = await responseEmptyStations.json();
+
+dataEmptyStations.forEach(estacion => {
+
+    const elemento = document.createElement('div'); // creamos el contenedor de cada una de las estaciones
+
+    elemento.textContent = estacion.name;
+    document.getElementById('empty-stations').appendChild(elemento);
+    i++;
+
+});
+
+
+
+// Mostramos todas las estaciones en la lista y filtramos
+const responseStations = await fetch('http://localhost:8080/stations');
+const dataStations = await responseStations.json();
+
+
+const input = document.getElementById("searchStation");
+input.addEventListener("input", () => {
+    const resultado = dataStations.filter(estacion => { // creamos un array con las estaciones que cumplen la condición de que el nombre contenga el valor del input
+    const elemento =  estacion.name[0].text;
+    if (elemento.toLowerCase().startsWith(input.value.toLowerCase())) { // si el elemento que ha escrito el usuario coincide con el contenido de un nombre
+        return true; // lo mantenemos en el array
+    }
+   
+});
+    createStationElement(resultado); // creamos el contenedor con el nombre
+});
+
+// Función para crear los elementos de las estaciones filtradas
+function createStationElement(resultado) {
+    const stationsContainer = document.getElementById('stations');
+    stationsContainer.innerHTML = ''; // Limpiamos el contenedor antes de agregar los elementos filtrados
+    resultado.forEach(estacion => {
+        const elemento = document.createElement('button'); // creamos un botón para cada estación
+
+        elemento.textContent = estacion.name[0].text;
+        stationsContainer.appendChild(elemento); // mostramos el nombre de la estacion en el contenedor
+
+        elemento.addEventListener("click", async () => {
+                    
+        const responseStation = await fetch(`http://localhost:8080/station/${estacion.station_id}`); // guardamos los datos de la estación en concreto
+        const dataStation = await responseStation.json();
+            console.log(estacion.station_id);
+
+
+        });
+    });
+}
+
+//cursor-pointer
